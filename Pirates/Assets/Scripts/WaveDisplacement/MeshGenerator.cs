fb7cd26a2b46dcaa537b407ed5dd2403ea78b424
lucas19919 <lucas.reimers07@gmail.com>
2025-11-12 20:45:24 +0100

    Updated Graphics and Ship Implementation

--- Pirates/Assets/Scripts/WaveDisplacement/MeshGenerator.cs
+++ Pirates/Assets/Scripts/WaveDisplacement/MeshGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour
 {
     [Header("Mesh Settings")]
-    [SerializeField] int meshWidth = 10; 
+    [SerializeField] int meshWidth = 10;
     [SerializeField] int meshLength = 10;
 
     [Header("Graphics")]
@@ -18,6 +18,7 @@ public class MeshGenerator : MonoBehaviour
 
     Vector3[] vertices;
     int[] triangles;
+    Vector2[] uvs;
     Mesh mesh;
 
     void Start()
@@ -43,6 +44,7 @@ public class MeshGenerator : MonoBehaviour
     {
         int vertexCount = meshWidth * meshLength;
         vertices = new Vector3[vertexCount];
+        uvs = new Vector2[vertexCount];
 
         int triangleCount = (meshWidth - 1) * (meshLength - 1) * 6;
         triangles = new int[triangleCount];
@@ -54,34 +56,35 @@ public class MeshGenerator : MonoBehaviour
             {
                 float y = 0f;
                 vertices[v] = new Vector3(x, y, z);
+                uvs[v] = new Vector2(
+                    (float)x / (meshWidth - 1),
+                    (float)z / (meshLength - 1)
+                );
                 v++;
             }
         }
 
         int t = 0;
         int vertIndex = 0;
-        for (int z = 0; z < meshLength - 1; z++) 
+        for (int z = 0; z < meshLength - 1; z++)
         {
-            for (int x = 0; x < meshWidth - 1; x++) 
+            for (int x = 0; x < meshWidth - 1; x++)
             {
-                // Get the 4 vertices that make up this quad
                 int bottomLeft = vertIndex;
                 int bottomRight = vertIndex + 1;
                 int topLeft = vertIndex + meshWidth;
                 int topRight = vertIndex + meshWidth + 1;
 
-                // First triangle (bottom-left, top-left, bottom-right)
                 triangles[t + 0] = bottomLeft;
                 triangles[t + 1] = topLeft;
                 triangles[t + 2] = bottomRight;
 
-                // Second triangle (bottom-right, top-left, top-right)
                 triangles[t + 3] = bottomRight;
                 triangles[t + 4] = topLeft;
                 triangles[t + 5] = topRight;
 
                 t += 6;
-                vertIndex++; 
+                vertIndex++;
             }
             vertIndex++;
         }
@@ -104,12 +107,14 @@ public class MeshGenerator : MonoBehaviour
 
     void UpdateMesh()
     {
-        mesh.Clear(); 
+        mesh.Clear();
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = uvs;
 
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
+        mesh.RecalculateTangents();
     }
 }
